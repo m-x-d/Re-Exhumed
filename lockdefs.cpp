4b4a50a68dd1763872ee0449e46da1843b7829e2
MaxED <j.maxed@gmail.com>
2016-10-24 17:37:55 +0300

    Added key titles to LOCKDEFS. .dbs files should be ignored.

--- lockdefs.cpp
+++ lockdefs.cpp
@@ -2,6 +2,7 @@ ClearLocks
 
 Lock 1
 {
+	//$Title "Time key"
 	ExKey1
 	Message "You need the Time key to open this door"
 	RemoteMessage "You need the Time key to activate this object"
@@ -10,6 +11,7 @@ Lock 1
 
 Lock 2
 {
+	//$Title "Power key"
 	ExKey2
 	Message "You need the Power key to open this door"
 	RemoteMessage "You need the Power key to activate this object"
@@ -18,6 +20,7 @@ Lock 2
 
 Lock 3
 {
+	//$Title "Earth key"
 	ExKey3
 	Message "You need the Earth key to open this door"
 	RemoteMessage "You need the Earth key to activate this object"
@@ -26,6 +29,7 @@ Lock 3
 
 Lock 4
 {
+	//$Title "War key"
 	ExKey4
 	Message "You need the War key to open this door"
 	RemoteMessage "You need the War key to activate this object"
